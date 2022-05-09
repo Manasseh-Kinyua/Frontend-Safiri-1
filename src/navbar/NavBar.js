ca19421ebbb615cfd0b2d57e3f1669668cb639c2
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-05-09 08:05:01 +0300

    add routing to the navbar

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
                     <h1 id="logo">LOGO</h1>
                 </div>
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
+                    <li><a href="/dashboard">Services</a></li>
                     <li><a href="#">Contact</a></li>
                     <li><a href="#">LogIn/Sign Up</a></li>
                 </ul>
